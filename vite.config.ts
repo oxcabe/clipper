01b3a2166b3e58e2dfeb7d21ad7ac5f13f4b391c
Óscar Carrasco <me@oxca.be>
2025-03-06 15:50:07 +0000

    fix: add cross-origin isolation through coi-serviceworker

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath } from 'node:url';
+const externalId = fileURLToPath(
+  new URL(
+  'coi-serviceworker.min.js',
+  import.meta.url
+  )
+);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   optimizeDeps: {
-    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
   },
-  server: {
-    headers: {
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-      'Cross-Origin-Opener-Policy': 'same-origin'
-    }
-  }
 });
